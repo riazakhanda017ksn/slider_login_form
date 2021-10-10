e11e75bf41e1d23d1898e1dcee691e9f030e972c
riazakhanda017ksn <riazkahanda@gmail.com>
2021-10-10 12:18:03 -0700

    master

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const mainContainer = document.getElementById("mainContainer");
 
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
+signUpButton?.addEventListener("onClick", () => {
+  mainContainer.classList.add("right-panel-active");
 });
 
-sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
+signInButton?.addEventListener("onClick", () => {
+  mainContainer.classList.remove("right-panel-active");
 });
